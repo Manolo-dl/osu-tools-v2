790858a585e9a7868fd8844db9492099ca27d8f4
manolo <manueldl12062003@gmail.com>
2026-05-30 22:49:49 +0200

    added beatmap entity

---
+++ src/entities/beatmap/beatmap.model.ts
@@ -0,0 +1,18 @@
+export interface Beatmap {
+    id: number;
+    title: string;
+    artist: string;
+    creator: string;
+    coverUrl: string;
+    status: 'ranked' | 'loved' | 'pending' | 'graveyard' | 'qualified';
+}
+
+export interface DownloadItem {
+    beatmapSetId: number;
+    title: string;
+    artist: string;
+    status: DownloadStatus;
+    progress: number;
+}
+
+export type DownloadStatus = 'pending' | 'dowloading' | 'done' | 'failed';
---
+++ src/entities/beatmap/index.ts
@@ -0,0 +1,1 @@
+export type { Beatmap, DownloadItem, DownloadStatus } from './beatmap.model';
