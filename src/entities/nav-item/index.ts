b123e1397bf41a6895ec9a1bf657c8648d20a719
manolo <manueldl12062003@gmail.com>
2026-05-30 17:21:51 +0200

    added sidebar

---
+++ src/entities/nav-item/index.ts
@@ -0,0 +1,2 @@
+export { NAV_ITEMS } from './nav-item.config';
+export type { NavItem } from './nav-item.model';
--- src/entities/nav-item/nav-item.config.ts
+++ src/entities/nav-item/nav-item.config.ts
@@ -1,8 +1,8 @@
 import { NavItem } from './nav-item.model';
 
 export const NAV_ITEMS: NavItem[] = [
-    { label: 'Home', route: '/', icon: 'home' },
-    { label: 'Downloader', route: '/downloader', icon: 'download' },
-    { label: 'Export Collections', route: '/export-collections', icon: 'folder' },
-    { label: 'Export Song Folder', route: '/export-song-folder', icon: 'music' }
+    { label: 'Home', route: '/', icon: 'phosphorHouse' },
+    { label: 'Downloader', route: '/downloader', icon: 'phosphorDownloadSimple' },
+    { label: 'Export Collections', route: '/export-collections', icon: 'phosphorFolder' },
+    { label: 'Export Song Folder', route: '/export-song-folder', icon: 'phosphorMusicNote' }
 ];
