cf440bf6a32af8e0323a0c0dab7c499014795a56
manolo <manueldl12062003@gmail.com>
2026-05-30 22:33:41 +0200

    added login in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "@widgets/header"; 
 import { SidebarComponent } from "@widgets/sidebar";
 import { OsuPathComponent } from "@widgets/osu-path"; 
+import { AuthService } from "@shared/services";
 
 @Component({
   selector: "app-root",
@@ -10,4 +11,10 @@ import { OsuPathComponent } from "@widgets/osu-path";
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  authService = inject(AuthService);
+
+  ngOnInit() {
+    this.authService.loadPersistedUser();
+  }
+}
