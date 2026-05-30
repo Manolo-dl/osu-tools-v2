6bf36a87479cc6e574db09b8d5914f05a100f99d
manolo <manueldl12062003@gmail.com>
2026-05-30 23:31:53 +0200

    added base for beatmap feature components

---
+++ src/features/beatmap-search/ui/beatmap-import/beatmap-import.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beatmap-import',
+  imports: [],
+  templateUrl: './beatmap-import.component.html',
+  styleUrl: './beatmap-import.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BeatmapImportComponent {}
---
+++ src/features/beatmap-search/ui/beatmap-queue/beatmap-queue.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beatmap-queue',
+  imports: [],
+  templateUrl: './beatmap-queue.component.html',
+  styleUrl: './beatmap-queue.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BeatmapQueueComponent {}
