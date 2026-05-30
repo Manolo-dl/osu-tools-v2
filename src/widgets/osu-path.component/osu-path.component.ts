5ac9b6bcd5d8c5808cc4921bb2cdac08766c62b4
manolo <manueldl12062003@gmail.com>
2026-05-30 19:08:54 +0200

    added osu-path component

---
+++ src/widgets/osu-path.component/osu-path.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-osu-path.component',
+  imports: [],
+  templateUrl: './osu-path.component.html',
+  styleUrl: './osu-path.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class OsuPathComponent {}
