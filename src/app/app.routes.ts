b756f29a2f9e7af86ed24719ffecccee09eb1806
manolo <manueldl12062003@gmail.com>
2026-05-30 15:09:21 +0200

    page: added downloader page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         path: '',
         loadComponent: () => import('@pages/home').then(m => m.HomeComponent)
+    },
+    {
+        path: 'downloader',
+        loadComponent: () => import('@pages/downloader').then(m => m.DownloaderComponent)
     }
 ];
