cdad38def36e5ca42ce401f52ae728babbd0cb1d
manolo <manueldl12062003@gmail.com>
2026-05-30 07:18:21 +0200

    feat: app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,12 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { invoke } from "@tauri-apps/api/core";
+import { HeaderComponent } from "@widgets/header"; 
+import { SidebarComponent } from "@widgets/sidebar";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
-export class AppComponent {
-  greetingMessage = "";
-
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
-
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-  }
-}
+export class AppComponent {}
