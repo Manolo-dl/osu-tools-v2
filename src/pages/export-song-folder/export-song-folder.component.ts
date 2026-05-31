db8666bdb9417e519d02d9bf6b62900bce3326d5
manolo <manueldl12062003@gmail.com>
2026-05-31 22:44:14 +0200

    added not implemented component

--- src/pages/export-song-folder/export-song-folder.component.ts
+++ src/pages/export-song-folder/export-song-folder.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NotImplementedComponent } from '@shared/ui/not-implemented';
 
 @Component({
   selector: 'app-export-song-folder',
-  imports: [],
+  imports: [NotImplementedComponent],
   templateUrl: './export-song-folder.component.html',
   styleUrl: './export-song-folder.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
