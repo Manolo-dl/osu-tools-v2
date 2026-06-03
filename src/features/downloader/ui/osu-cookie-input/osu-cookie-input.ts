72479e1c15c2555318469e29b56ddd6aea1fd182
manolo <manueldl12062003@gmail.com>
2026-06-03 19:23:13 +0200

    created osu cookie input component

---
+++ src/features/downloader/ui/osu-cookie-input/osu-cookie-input.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-osu-cookie-input',
+  imports: [],
+  templateUrl: './osu-cookie-input.html',
+  styleUrl: './osu-cookie-input.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class OsuCookieInput {}
