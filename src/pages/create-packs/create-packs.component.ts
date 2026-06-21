31119e4302a3678c70e647d45ebc149f90629a25
manolo <manueldl12062003@gmail.com>
2026-06-21 17:25:44 +0200

    created layout for create packs page

--- src/pages/create-packs/create-packs.component.ts
+++ src/pages/create-packs/create-packs.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PackSearchInputComponent, PackBrowserComponent, PackPreviewComponent, PackDetailsFormComponent, CreatePackButtonComponent } from "@features/pack-creator";
 
 @Component({
   selector: 'app-create-packs',
-  imports: [],
+  imports: [PackSearchInputComponent, PackBrowserComponent, PackPreviewComponent, PackDetailsFormComponent, CreatePackButtonComponent],
   templateUrl: './create-packs.component.html',
   styleUrl: './create-packs.component.css',
 })
