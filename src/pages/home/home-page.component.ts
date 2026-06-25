c637a43967cc8fcbf7905d83da05da45910d1d7e
manolo <manueldl12062003@gmail.com>
2026-06-25 02:08:32 +0200

    cleaning home page

--- src/pages/home/home-page.component.ts
+++ src/pages/home/home-page.component.ts
@@ -1,16 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { phosphorDownloadSimple, phosphorFolder, phosphorMusicNote } from '@ng-icons/phosphor-icons/regular';
-import { OsuPathStore } from '@shared/stores'; 
+import { OsuPathStore } from '@shared/stores';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, NgIcon],
+  imports: [],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [provideIcons({ phosphorDownloadSimple, phosphorFolder, phosphorMusicNote })],
 })
 export class HomePageComponent {
   readonly osuPath = inject(OsuPathStore);
