743320a5cc6455d3f8026c2badcbe86d9b9faaca
manolo <manueldl12062003@gmail.com>
2026-07-30 04:55:09 +0200

    modified tosu.model for typed variables

--- src/entities/tosu/tosu.model.ts
+++ src/entities/tosu/tosu.model.ts
@@ -1,3 +1,48 @@
 export interface TosuData {
+    beatmap: {
+        artist: string;
+        artistUnicode: string;
+        mapper: string;
+        mode: { name: string; number: number };
+        stats: {
+            ar: { converted: number, original: number };
+            bpm: { common: number, min: number, max: number, realtime: number };
+            cs: { converted: number, original: number };
+            hp: { converted: number, original: number };
+            od: { converted: number, original: number };
+            stars: {
+                hitwindow: number;
+                live: number;
+                total: number;
+            };
+        },
+        status: { name: string, number: number };
+        time: {
+            firstObject: number;
+            lastObject: number;
+            live: number;
+            mp3Length: number;
+        };
+        title: string;
+        titleUnicode: string;
+        version: string;
+    };
+
+    client: string;
+
+    directPath: {
+        beatmapAudio: string;
+        beatmapBackground: string;
+        beatmapFile: string;
+        beatmapFolder: string;
+        skinFolder: string;
+    };
+
+    files: {
+        audio: string;
+        background: string;
+        beatmap: string;
+    };
+    
     [key: string]: unknown;
 }
