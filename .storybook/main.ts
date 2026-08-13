e5aedcc4d97d3346775ed3da27550de7929828a8
manolo <manueldl12062003@gmail.com>
2026-08-13 22:32:20 +0200

    modified storybook, created first components

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,9 @@
 import type { StorybookConfig } from '@storybook/angular-vite';
+import path from 'path/posix';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
   "stories": [
@@ -23,6 +28,18 @@ const config: StorybookConfig = {
         "."
       ]
     }
-  }
+  },
+  async viteFinal(config) {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@shared': path.resolve(__dirname, '../src/shared'),
+      '@entities': path.resolve(__dirname, '../src/entities'),
+      '@features': path.resolve(__dirname, '../src/features'),
+      '@widgets': path.resolve(__dirname, '../src/widgets'),
+      '@pages': path.resolve(__dirname, '../src/pages'),
+    };
+    return config;
+  },
 };
 export default config;
