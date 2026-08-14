88b22eb0603c3495368a7d305f9b4a5dfcfe6765
manolo <manueldl12062003@gmail.com>
2026-08-14 18:46:27 +0200

    removed distribution notice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,13 @@ import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "@widgets/header";
 import { SidebarComponent } from "@widgets/sidebar";
 import { OsuPathComponent } from "@widgets/osu-path";
-import { DistributionNoticeComponent } from "@widgets/distribution-notice";
 import { ToastComponent } from "@widgets/toast";
 import { ThemeService } from "@shared/services";
 import { PlatformStore } from "@shared/stores";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, OsuPathComponent, ToastComponent, DistributionNoticeComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, OsuPathComponent, ToastComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
--- src/widgets/distribution-notice/index.ts
+++
@@ -1,1 +0,0 @@
-export { DistributionNoticeComponent } from './distribution-notice.component';
