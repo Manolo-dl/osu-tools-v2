e3547ceb970a695d371e18024bae86f13ad69e06
manolo <manueldl12062003@gmail.com>
2026-08-15 20:50:08 +0200

    design window controls

--- src/widgets/window-controls/window-controls.component.ts
+++ src/widgets/window-controls/window-controls.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { getCurrentWindow } from '@tauri-apps/api/window';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faMinimize, faMaximize, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { TuiButton } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-window-controls',
-  imports: [],
+  imports: [FaIconComponent, TuiButton],
   templateUrl: './window-controls.component.html',
   styleUrl: './window-controls.component.css',
 })
 export class WindowControlsComponent {
 
   readonly window = getCurrentWindow();
+
+  readonly icons = {
+    faMinimize,
+    faMaximize,
+    faXmark,
+  }
 }
