43359ec9d774c2339172e060fdb7549827354801
manolo <manueldl12062003@gmail.com>
2026-08-18 21:05:37 +0200

    home-page

--- src/pages/home/home-page.component.ts
+++ src/pages/home/home-page.component.ts
@@ -1,13 +1,15 @@
+import { KeyValuePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { OsuPathStore } from '@shared/stores';
+import { TuiTitle, TuiCell, TuiExpand } from "@taiga-ui/core";
+import { TuiAccordionComponent, TuiAccordionDirective } from "@taiga-ui/kit";
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css',
+  styleUrl: './home-page.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePageComponent {
-  readonly osuPath = inject(OsuPathStore);
 }
