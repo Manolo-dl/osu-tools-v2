f48ddc24f1b2b3a11a44e65cb2e5a4279571c242
manolo <manueldl12062003@gmail.com>
2026-08-19 21:02:38 +0200

    starting home page with new layout

--- src/pages/home/home-page.component.ts
+++ src/pages/home/home-page.component.ts
@@ -1,8 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiTitle, TuiNotificationDirective, TuiButton, TuiAppearance } from "@taiga-ui/core";
+import { TuiSurface } from '@taiga-ui/layout'
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TuiTitle, TuiNotificationDirective, TuiButton, TuiSurface, TuiAppearance, FaIconComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
