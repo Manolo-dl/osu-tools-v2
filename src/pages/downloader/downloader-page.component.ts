87f87f5e6edbb9b10c66eab1db8952bad35f1d30
manolo <manueldl12062003@gmail.com>
2026-08-21 21:36:38 +0200

    started downloader page

--- src/pages/downloader/downloader-page.component.ts
+++ src/pages/downloader/downloader-page.component.ts
@@ -2,12 +2,13 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { BeatmapImportComponent, BeatmapQueueComponent } from "@features/beatmap-search";
 import { OsuCookieInputComponent } from '@features/downloader';
 import { OsuDbStore } from '@entities/osu-db';
+import { TuiTitle, TuiNotificationDirective, TuiButton } from "@taiga-ui/core";
 
 @Component({
   selector: 'app-downloader',
-  imports: [BeatmapImportComponent, BeatmapQueueComponent, OsuCookieInputComponent],
+  imports: [BeatmapImportComponent, BeatmapQueueComponent, OsuCookieInputComponent, TuiTitle, TuiNotificationDirective, TuiButton],
   templateUrl: './downloader-page.component.html',
-  styleUrl: './downloader-page.component.css',
+  styleUrl: './downloader-page.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DownloaderPageComponent {
