cabc2b5a5005cbf4d43cdcdabf80709bac6072eb
Manolo <manueldl12062003@gmail.com>
2026-08-22 22:28:58 +0200

    added accordion in downloader page

--- src/pages/downloader/downloader-page.component.ts
+++ src/pages/downloader/downloader-page.component.ts
@@ -3,10 +3,21 @@ import { BeatmapImportComponent, BeatmapQueueComponent } from "@features/beatmap
 import { OsuCookieInputComponent } from '@features/downloader';
 import { OsuDbStore } from '@entities/osu-db';
 import { TuiTitle, TuiNotificationDirective, TuiButton } from "@taiga-ui/core";
+import { TuiAccordion } from '@taiga-ui/kit';
+
+interface PageInfo {
+  title: string,
+  steps: Step[]
+}
+
+interface Step {
+  desc: string,
+  image?: string
+}
 
 @Component({
   selector: 'app-downloader',
-  imports: [BeatmapImportComponent, BeatmapQueueComponent, OsuCookieInputComponent, TuiTitle, TuiNotificationDirective, TuiButton],
+  imports: [BeatmapImportComponent, BeatmapQueueComponent, OsuCookieInputComponent, TuiTitle, TuiNotificationDirective, TuiButton, TuiAccordion],
   templateUrl: './downloader-page.component.html',
   styleUrl: './downloader-page.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,4 +28,47 @@ export class DownloaderPageComponent {
   async loadLibrary() {
     await this.osuDb.load();
   }
+
+  readonly downloaderInfo: PageInfo[] = [
+    {
+      title: 'How to use the downloader',
+      steps: [
+        {
+          desc: 'Get the osu session cookie (check how to get osu session step)'
+        },
+        {
+          desc: 'Put the osu session in the input'
+        },
+        {
+          desc: 'Drop or select a txt file containing all download links (check file format step)'
+        },
+        {
+          desc: 'In the queue there will be a list with all the map IDs click start'
+        }
+      ]
+    },
+    {
+      title: 'How to get osu session',
+      steps: [
+        {
+          desc: 'Go to your browser',
+        },
+        {
+          desc: 'Go to the osu page, make sure you are logged'
+        },
+        {
+          desc: 'Right click and choose inspect',
+          image: 'assets/images/downloader/step_3.png'
+        },
+        {
+          desc: 'Go to Application or Storage tab',
+          image: 'assets/images/downloader/step_4.png'
+        },
+        {
+          desc: 'Go to cookies -> https://osu.ppy.sh and copy the value of the cookie with the name of osu_session',
+          image: 'assets/images/downloader/step_5.png',
+        }
+      ]
+    }
+  ]
 }
